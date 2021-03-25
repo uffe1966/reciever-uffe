35e211113ad7ae52e167591f97dd86e1d586d5d1
uffe1966 <81055165+uffe1966@users.noreply.github.com>
2021-03-25 02:56:33 +0100

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    led.toggle(4, 4)
     serial.writeValue("Celsius", receivedNumber)
+    led.toggle(4, 4)
+    if (input.buttonIsPressed(Button.A)) {
+        serial.writeValue("A", 100)
+    } else if (input.buttonIsPressed(Button.B)) {
+        serial.writeValue("B", 100)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        serial.writeValue("A+B", 100)
+    } else {
+        Samle_antal_smiley()
+        Samle_smileys = [0 + 0, 300]
+    }
 })
+function Samle_antal_smiley () {
+	
+}
+let Samle_smileys: number[] = []
 basic.showString("Reciever")
 radio.setGroup(55)
 basic.forever(function () {
