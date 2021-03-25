8aabbaf8553ed3cab1a92ab31c4e161752ea5886
uffe1966 <81055165+uffe1966@users.noreply.github.com>
2021-03-25 03:04:18 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     serial.writeValue("Celsius", receivedNumber)
+})
+function Samle_antal_smiley () {
+	
+}
+let Samle_smileys: number[] = []
+basic.showString("Reciever")
+radio.setGroup(55)
+basic.forever(function () {
+    serial.writeValue("Celsius", 0)
+    basic.pause(1000)
     led.toggle(4, 4)
     if (input.buttonIsPressed(Button.A)) {
         serial.writeValue("A", 100)
@@ -11,13 +21,4 @@ radio.onReceivedNumber(function (receivedNumber) {
         Samle_antal_smiley()
         Samle_smileys = [0 + 0, 300]
     }
-})
-function Samle_antal_smiley () {
-	
-}
-let Samle_smileys: number[] = []
-basic.showString("Reciever")
-radio.setGroup(55)
-basic.forever(function () {
-	
 })
